5621b0b572eace7b2dbd2fb4e0620749191e93f2
JinHan <hjh5488@gmail.com>
2018-02-26 21:19:09 +0900

    Refactor calculateService
    - strategy pattern을 적용하여 Dependency Injection을 받는 방식으로
    리팩토링

--- src/main/java/OopExample.java
+++ src/main/java/OopExample.java
@@ -5,23 +5,60 @@
 public class OopExample {
 
   public static void main(String[] args) {
-    final CalculatorService calculatorService = new CalculatorService();
-    final int additionResult = calculatorService.calculate('+',1, 1);
+    final CalculatorService calculatorService = new CalculatorService(new Addition());
+    final int additionResult = calculatorService.calculate(1, 1);
     System.out.println(additionResult);
 
-    final int subtractionResult = calculatorService.calculate('-', 1, 1);
+    final int subtractionResult = calculatorService.calculate(1, 1);
     System.out.println(subtractionResult);
   }
 }
 
+interface Calculation {
+  int calculate(int num1, int num2);
+}
+
+class Addition implements Calculation {
+
+  @Override
+  public int calculate(int num1, int num2) {
+    return num1 + num2;
+  }
+}
+
+class Subtraction implements Calculation {
+
+  @Override
+  public int calculate(int num1, int num2) {
+    return num1 - num2;
+  }
+}
+
+class Multiplication implements Calculation {
+
+  @Override
+  public int calculate(int num1, int num2) {
+    return num1 * num2;
+  }
+}
+
+class Division implements Calculation {
+
+  @Override
+  public int calculate(int num1, int num2) {
+    return num1 / num2;
+  }
+}
+
 class CalculatorService {
-  public int calculate(char calculation, int num1, int num2) {
-    if (calculation == '+') {
-     return num1 + num2;
-    } else  if (calculation == '-') {
-      return num1 - num2;
-    } else {
-      throw new IllegalArgumentException("Unknown calculation:" + calculation);
-    }
+  // composition
+  private final Calculation calculation;
+
+  CalculatorService(final Calculation calculation) {
+    this.calculation = calculation;
+  }
+
+  public int calculate(int num1, int num2) {
+    return calculation.calculate(num1, num2);
   }
 }
--- src/test/java/CalculatorServiceTest.java
+++ src/test/java/CalculatorServiceTest.java
@@ -9,20 +9,38 @@ import static org.assertj.core.api.Assertions.*;
 public class CalculatorServiceTest {
 
   @Test
-  public void testCalculateAddition() throws Exception {
-    CalculatorService calculatorService = new CalculatorService();
+  public void testCalculateAddition() {
+    Calculation calculation = new Addition();
 
-    final int actual = calculatorService.calculate('+', 1, 1);
+    final int actual = calculation.calculate(1, 1);
 
     assertThat(actual).isEqualTo(2);
   }
 
   @Test
-  public void testCalculateSubtraction() throws Exception {
-    CalculatorService calculatorService = new CalculatorService();
+  public void testCalculateSubtraction() {
+    Calculation calculation = new Subtraction();
 
-    final int actual = calculatorService.calculate('-', 1, 1);
+    final int actual = calculation.calculate(1, 1);
 
     assertThat(actual).isEqualTo(0);
+  }
+
+  @Test
+  public void testCalculateMultiplication() {
+    Calculation Calculation = new Multiplication();
+
+    final int actual = Calculation.calculate(2, 2);
+
+    assertThat(actual).isEqualTo(4);
+  }
+
+  @Test
+  public void testCalculateDivision() {
+    Calculation calculation = new Division();
+
+    final int actual = calculation.calculate(9, 3);
+
+    assertThat(actual).isEqualTo(3);
   }
 }
