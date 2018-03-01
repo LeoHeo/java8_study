3bd3baba228a33b36089f2072ba2e2f54ff25a8e
JinHan <hjh5488@gmail.com>
2018-03-01 13:46:21 +0900

    Added predicate 사용법
    https://youtu.be/qzUwveRjiEg
    
    조건문만 다른 boilerplate code에서 Predicate를 이용하여
    boilerplate code를 반복하여 작성하지 않아도 되는 장점을 소개

--- src/main/java/FunctionalInterfaceExamples.java
+++ src/main/java/FunctionalInterfaceExamples.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * @author Heo, Jin Han
@@ -25,6 +29,52 @@ public class FunctionalInterfaceExamples {
 
     greetings.accept("Leo");
     print.accept("test");
+
+    // Predicate<T>는 항상 boolean 리턴한다.
+    // Function<T, Boolean>이라고 볼 수 도 있겠으나
+    // Boolean은 Boxed primitive boolean이기 때문에
+    // Object 생성을 따로 할 필요없이 Predicate을 이용하면 좀 더 편하다.
+
+    Predicate<Integer> isPositive = i -> i > 0;
+    System.out.println(isPositive.test(1));
+    System.out.println(isPositive.test(0));
+    System.out.println(isPositive.test(-1));
+
+    List<Integer> numbers = Arrays.asList(-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5);
+
+    List<Integer> positiveNumbers = new ArrayList<>();
+    for (Integer num: numbers) {
+      if (isPositive.test(num)) {
+        positiveNumbers.add(num);
+      }
+    }
+    System.out.println("positive integers:" + positiveNumbers);
+
+    Predicate<Integer> lessThan3 = i -> i < 3;
+    List<Integer> numbersLessThan3 = new ArrayList<>();
+    for (Integer num: numbers) {
+      if (lessThan3.test(num)) {
+        numbersLessThan3.add(num);
+      }
+    }
+    System.out.println("less than 3 integers:" + numbersLessThan3);
+
+    System.out.println("filter positive integers" + filter(numbers, isPositive));
+    System.out.println("filter less than 3 integers" + filter(numbers, lessThan3));
+
+    System.out.println("filter lambda less than 3 integers" + filter(numbers, i -> i > 0));
+    System.out.println("filter lambda less than 3 integers" + filter(numbers, i -> i < 3));
+  }
+
+  private static <T> List<T> filter(List<T> list, Predicate<T> filter) {
+    List<T> result = new ArrayList<>();
+    for (T input: list) {
+      if (filter.test(input)) {
+        result.add(input);
+      }
+    }
+
+    return result;
   }
 
 }
